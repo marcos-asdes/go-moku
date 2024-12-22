8dafa559909083770a02b7190b4417ca1240b632
Marcos Antonio Silva do Espirito Santo <marcos.espirito@rededor.com.br>
2024-12-22 12:22:45 -0300

    chore: removido arquivo não utilizado

--- src/files/__init__.py
+++ src/files/__init__.py
@@ -1,4 +1,3 @@
 from .file_operations import save_game, load_game
 from .logging import log_error, log_history
 from .auto_save import auto_save_game
-from .argparse_functions import handle_auto_save_file
